57ba3f4be08d614ca23e9b3f6b52294a548ff6b8
Ivo Junior <135394705+IvoJunior0@users.noreply.github.com>
2024-04-15 22:13:35 -0300

    feat: animações nos links da navbar

--- index.js
+++ index.js
@@ -10,13 +10,19 @@ function abrirMenu() {
     }
     menuMobile.classList.add("active");
     hamburger.classList.add("fa-xmark");
+    menuLinks.forEach(el => {
+        el.classList.add("animation-links");
+    })
     menuAtivo = true;
 }
 
 function fecharMenu() {
     menuMobile.classList.remove("active");
     hamburger.classList.remove("fa-xmark");
-    menuAtivo = false
+    menuLinks.forEach(el => {
+        el.classList.remove("animation-links");
+    })
+    menuAtivo = false;
 }
 
 menuLinks.forEach(element => {
